b608c99c479efb65885d474ca1aec76dcf197c74
Twugo <hm17031@student.miyazaki-u.ac.jp>
2021-12-07 03:22:36 +0900

    Update: フロントとのCORS実装

--- express-app/app.js
+++ express-app/app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var frontURL = 'http://localhost:3000';
 var app = express();
+app.use(cors({
+  origin: frontURL,
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +30,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
